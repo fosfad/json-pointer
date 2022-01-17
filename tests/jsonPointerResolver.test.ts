fefc141aefdf323c41de050126bf4c0770c24586
Petr <petr@flaks.dev>
2022-01-17 13:15:09 +0300

    refactor(tests): improve tests
    Add tests for exceptions thrown by `resolveJsonPointer` method (closes #4).
    Add tests for accessing numeric object fields in JSON (closes #3).

--- tests/jsonPointerResolver.test.ts
+++ tests/jsonPointerResolver.test.ts
@@ -1,51 +1,140 @@
-import { resolveJsonPointer } from '../src/jsonPointerResolver';
+import {
+  PointerReferencesNonexistentValue,
+  resolveJsonPointer,
+} from '../src/jsonPointerResolver';
 import { JsonPointer } from '../src/jsonPointer';
-
-const jsonFromSpec = {
-  foo: ['bar', 'baz'],
-  '': 0,
-  'a/b': 1,
-  'c%d': 2,
-  'e^f': 3,
-  'g|h': 4,
-  'i\\j': 5,
-  'k"l': 6,
-  ' ': 7,
-  'm~n': 8,
-};
 
 /**
  * Following test cases were taken from {@link https://datatracker.ietf.org/doc/html/rfc6901 | JavaScript Object Notation (JSON) Pointer (RFC 6901) specification}.
  */
-test.each<[string, unknown]>([
-  ['#', jsonFromSpec],
-  ['#/foo', ['bar', 'baz']],
-  ['#/foo/0', 'bar'],
-  ['#/', 0],
-  ['#/a~1b', 1],
-  ['#/c%25d', 2],
-  ['#/e%5Ef', 3],
-  ['#/g%7Ch', 4],
-  ['#/i%5Cj', 5],
-  ['#/k%22l', 6],
-  ['#/%20', 7],
-  ['#/m~0n', 8],
-  ['', jsonFromSpec],
-  ['/foo', ['bar', 'baz']],
-  ['/foo/0', 'bar'],
-  ['/', 0],
-  ['/a~1b', 1],
-  ['/c%d', 2],
-  ['/e^f', 3],
-  ['/g|h', 4],
-  ['/i\\j', 5],
-  ['/k"l', 6],
-  ['/ ', 7],
-  ['/m~0n', 8],
-])('JSON Pointer `%s`', (absoluteJsonPointer, expectedValue) => {
-  const json = jsonFromSpec;
+describe('Positive test cases from the specification', () => {
+  const jsonHaystack = {
+    foo: ['bar', 'baz'],
+    '': 0,
+    'a/b': 1,
+    'c%d': 2,
+    'e^f': 3,
+    'g|h': 4,
+    'i\\j': 5,
+    'k"l': 6,
+    ' ': 7,
+    'm~n': 8,
+  };
 
-  expect(
-    resolveJsonPointer(json, JsonPointer.createFromString(absoluteJsonPointer)),
-  ).toEqual(expectedValue);
+  /**
+   * JSON String Representation of JSON Pointers.
+   */
+  test.each<[string, unknown]>([
+    ['', jsonHaystack],
+    ['/foo', ['bar', 'baz']],
+    ['/foo/0', 'bar'],
+    ['/', 0],
+    ['/a~1b', 1],
+    ['/c%d', 2],
+    ['/e^f', 3],
+    ['/g|h', 4],
+    ['/i\\j', 5],
+    ['/k"l', 6],
+    ['/ ', 7],
+    ['/m~0n', 8],
+  ])('JSON Pointer `%s`', (absoluteJsonPointer, expectedValue) => {
+    expect(
+      resolveJsonPointer(
+        jsonHaystack,
+        JsonPointer.createFromString(absoluteJsonPointer),
+      ),
+    ).toEqual(expectedValue);
+  });
+
+  /**
+   * URI Fragment Identifier Representation of JSON Pointers.
+   */
+  test.each<[string, unknown]>([
+    ['#', jsonHaystack],
+    ['#/foo', ['bar', 'baz']],
+    ['#/foo/0', 'bar'],
+    ['#/', 0],
+    ['#/a~1b', 1],
+    ['#/c%25d', 2],
+    ['#/e%5Ef', 3],
+    ['#/g%7Ch', 4],
+    ['#/i%5Cj', 5],
+    ['#/k%22l', 6],
+    ['#/%20', 7],
+    ['#/m~0n', 8],
+  ])('JSON Pointer `%s`', (absoluteJsonPointer, expectedValue) => {
+    expect(
+      resolveJsonPointer(
+        jsonHaystack,
+        JsonPointer.createFromString(absoluteJsonPointer),
+      ),
+    ).toEqual(expectedValue);
+  });
+});
+
+describe('Custom test cases', () => {
+  const jsonHaystack = {
+    foo: {
+      1: 'bar',
+      5: 'baz',
+    },
+  };
+
+  describe('Positive test cases', () => {
+    /**
+     * JSON String Representation of JSON Pointers.
+     */
+    test.each<[string, unknown]>([
+      ['/foo/1', 'bar'],
+      ['/foo/5', 'baz'],
+    ])('JSON Pointer `%s`', (absoluteJsonPointer, expectedValue) => {
+      expect(
+        resolveJsonPointer(
+          jsonHaystack,
+          JsonPointer.createFromString(absoluteJsonPointer),
+        ),
+      ).toEqual(expectedValue);
+    });
+  });
+
+  describe('Negative test cases', () => {
+    /**
+     * JSON String Representation of JSON Pointers.
+     */
+    test.each<[string, string]>([
+      ['/foo/-1', '/foo/-1'],
+      ['/foo/', '/foo/'],
+      ['/foo/bar/baz', '/foo/bar'],
+    ])(
+      'JSON Pointer `%s`',
+      (absoluteJsonPointer, nonexistentValueJsonPointer) => {
+        try {
+          resolveJsonPointer(
+            jsonHaystack,
+            JsonPointer.createFromString(absoluteJsonPointer),
+          );
+        } catch (error: unknown) {
+          // eslint-disable-next-line jest/no-conditional-expect
+          expect(error).toBeInstanceOf(PointerReferencesNonexistentValue);
+
+          if (error instanceof PointerReferencesNonexistentValue) {
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(error.jsonPointer.toString()).toBe(absoluteJsonPointer);
+
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(error.nonexistentValueJsonPointer.toString()).toBe(
+              nonexistentValueJsonPointer,
+            );
+          }
+        }
+
+        expect(() => {
+          resolveJsonPointer(
+            jsonHaystack,
+            JsonPointer.createFromString(absoluteJsonPointer),
+          );
+        }).toThrow(PointerReferencesNonexistentValue);
+      },
+    );
+  });
 });
