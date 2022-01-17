8fcd1bdca3ff1f13cca5e2a2ad17b45e09f4216f
Petr <petr@flaks.dev>
2022-01-17 12:33:58 +0300

    refactor(tests): clean up jsonPointerResolver.test.ts

--- tests/jsonPointerResolver.test.ts
+++ tests/jsonPointerResolver.test.ts
@@ -1,25 +1,24 @@
 import { resolveJsonPointer } from '../src/jsonPointerResolver';
 import { JsonPointer } from '../src/jsonPointer';
 
+const jsonFromSpec = {
+  foo: ['bar', 'baz'],
+  '': 0,
+  'a/b': 1,
+  'c%d': 2,
+  'e^f': 3,
+  'g|h': 4,
+  'i\\j': 5,
+  'k"l': 6,
+  ' ': 7,
+  'm~n': 8,
+};
+
 /**
  * Following test cases were taken from {@link https://datatracker.ietf.org/doc/html/rfc6901 | JavaScript Object Notation (JSON) Pointer (RFC 6901) specification}.
  */
 test.each<[string, unknown]>([
-  [
-    '#',
-    {
-      foo: ['bar', 'baz'],
-      '': 0,
-      'a/b': 1,
-      'c%d': 2,
-      'e^f': 3,
-      'g|h': 4,
-      'i\\j': 5,
-      'k"l': 6,
-      ' ': 7,
-      'm~n': 8,
-    },
-  ],
+  ['#', jsonFromSpec],
   ['#/foo', ['bar', 'baz']],
   ['#/foo/0', 'bar'],
   ['#/', 0],
@@ -31,21 +30,7 @@ test.each<[string, unknown]>([
   ['#/k%22l', 6],
   ['#/%20', 7],
   ['#/m~0n', 8],
-  [
-    '',
-    {
-      foo: ['bar', 'baz'],
-      '': 0,
-      'a/b': 1,
-      'c%d': 2,
-      'e^f': 3,
-      'g|h': 4,
-      'i\\j': 5,
-      'k"l': 6,
-      ' ': 7,
-      'm~n': 8,
-    },
-  ],
+  ['', jsonFromSpec],
   ['/foo', ['bar', 'baz']],
   ['/foo/0', 'bar'],
   ['/', 0],
@@ -58,18 +43,7 @@ test.each<[string, unknown]>([
   ['/ ', 7],
   ['/m~0n', 8],
 ])('JSON Pointer `%s`', (absoluteJsonPointer, expectedValue) => {
-  const json = {
-    foo: ['bar', 'baz'],
-    '': 0,
-    'a/b': 1,
-    'c%d': 2,
-    'e^f': 3,
-    'g|h': 4,
-    'i\\j': 5,
-    'k"l': 6,
-    ' ': 7,
-    'm~n': 8,
-  };
+  const json = jsonFromSpec;
 
   expect(
     resolveJsonPointer(json, JsonPointer.createFromString(absoluteJsonPointer)),
