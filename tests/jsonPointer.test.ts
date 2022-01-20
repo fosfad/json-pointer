1b3837149d7584494eb448de4f52a916f8e12311
Petr Flaks <petr@flaks.dev>
2022-01-20 17:42:50 +0300

    test: add tests for `InvalidPointerSyntax` exception

--- tests/jsonPointer.test.ts
+++ tests/jsonPointer.test.ts
@@ -1,50 +1,62 @@
 import type { JsonPointer } from '../src/jsonPointer';
-import { parseJsonPointerFromString } from '../src/jsonPointer';
+import {
+  InvalidPointerSyntax,
+  parseJsonPointerFromString,
+} from '../src/jsonPointer';
 
 describe('parseJsonPointerFromString function', () => {
-  /**
-   * JSON String Representation of JSON Pointers.
-   */
-  test.each<[string, Array<string>]>([
-    ['', []],
-    ['/foo', ['foo']],
-    ['/foo/0', ['foo', '0']],
-    ['/', ['']],
-    ['/a~1b', ['a/b']],
-    ['/c%d', ['c%d']],
-    ['/e^f', ['e^f']],
-    ['/g|h', ['g|h']],
-    ['/i\\j', ['i\\j']],
-    ['/k"l', ['k"l']],
-    ['/ ', [' ']],
-    ['/m~0n', ['m~n']],
-  ])('JSON Pointer `%s`', (jsonPointer, expectedValue) => {
-    expect(parseJsonPointerFromString(jsonPointer)).toEqual<JsonPointer>({
-      referenceTokens: expectedValue,
-      uriFragmentIdentifierRepresentation: false,
+  describe('JSON String Representation of JSON Pointers', () => {
+    test.each<[string, Array<string>]>([
+      ['', []],
+      ['/foo', ['foo']],
+      ['/foo/0', ['foo', '0']],
+      ['/', ['']],
+      ['/a~1b', ['a/b']],
+      ['/c%d', ['c%d']],
+      ['/e^f', ['e^f']],
+      ['/g|h', ['g|h']],
+      ['/i\\j', ['i\\j']],
+      ['/k"l', ['k"l']],
+      ['/ ', [' ']],
+      ['/m~0n', ['m~n']],
+    ])('JSON Pointer `%s`', (jsonPointer, expectedValue) => {
+      expect(parseJsonPointerFromString(jsonPointer)).toEqual<JsonPointer>({
+        referenceTokens: expectedValue,
+        uriFragmentIdentifierRepresentation: false,
+      });
     });
   });
 
-  /**
-   * URI Fragment Identifier Representation of JSON Pointers.
-   */
-  test.each<[string, Array<string>]>([
-    ['#', []],
-    ['#/foo', ['foo']],
-    ['#/foo/0', ['foo', '0']],
-    ['#/', ['']],
-    ['#/a~1b', ['a/b']],
-    ['#/c%25d', ['c%d']],
-    ['#/e%5Ef', ['e^f']],
-    ['#/g%7Ch', ['g|h']],
-    ['#/i%5Cj', ['i\\j']],
-    ['#/k%22l', ['k"l']],
-    ['#/%20', [' ']],
-    ['#/m~0n', ['m~n']],
-  ])('JSON Pointer `%s`', (jsonPointer, expectedValue) => {
-    expect(parseJsonPointerFromString(jsonPointer)).toEqual<JsonPointer>({
-      referenceTokens: expectedValue,
-      uriFragmentIdentifierRepresentation: true,
+  describe('URI Fragment Identifier Representation of JSON Pointers', () => {
+    test.each<[string, Array<string>]>([
+      ['#', []],
+      ['#/foo', ['foo']],
+      ['#/foo/0', ['foo', '0']],
+      ['#/', ['']],
+      ['#/a~1b', ['a/b']],
+      ['#/c%25d', ['c%d']],
+      ['#/e%5Ef', ['e^f']],
+      ['#/g%7Ch', ['g|h']],
+      ['#/i%5Cj', ['i\\j']],
+      ['#/k%22l', ['k"l']],
+      ['#/%20', [' ']],
+      ['#/m~0n', ['m~n']],
+    ])('JSON Pointer `%s`', (jsonPointer, expectedValue) => {
+      expect(parseJsonPointerFromString(jsonPointer)).toEqual<JsonPointer>({
+        referenceTokens: expectedValue,
+        uriFragmentIdentifierRepresentation: true,
+      });
     });
+  });
+
+  describe('InvalidPointerSyntax error', () => {
+    test.each<[string]>([['foo'], ['#foo'], ['foo/'], ['#foo/'], ['##/']])(
+      'JSON Pointer `%s`',
+      (jsonPointer) => {
+        expect(() => {
+          parseJsonPointerFromString(jsonPointer);
+        }).toThrowError(InvalidPointerSyntax);
+      },
+    );
   });
 });
